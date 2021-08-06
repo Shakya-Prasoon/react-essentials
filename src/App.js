ac37b1e9e812de64f0ffde53cf3809f3d7e6873d
Prasoon Shakya <pras95shakya@gmail.com>
2021-08-06 11:45:45 -0700

    fixed syntax

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-
+import React from 'react'
 import './App.css';
+
+
+function Header(){
+  return(
+    <header>
+      <h1>Pras's Webpage</h1>
+    </header>
+  )
+}
+function Main(){
+  return(
+    <section>
+      <p>Learning React</p>
+    </section>
+  )
+}
+function Footer(){
+  return(
+    <footer>
+      <h3>Byee</h3>
+    </footer>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
