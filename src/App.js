49c9ba1e40c3f46e9f6bff4ea463c619dc21827b
Prasoon Shakya <pras95shakya@gmail.com>
2021-08-06 12:37:54 -0700

    useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,37 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
+// function SecretComponent( ) {
+//   return <h1>Secret information authorized users only</h1>
+// }
+// function RegularComponent( ) {
+//   return <h1>Everyone can see this</h1>
+// }
 
-function Header(){
-  return(
-    <header>
-      <h1>Pras's Webpage</h1>
-    </header>
-  )
-}
-function Main(){
-  return(
-    <section>
-      <p>Learning React</p>
-    </section>
-  )
-}
-function Footer(){
-  return(
-    <footer>
-      <h3>Byee</h3>
-    </footer>
-  )
-}
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
+  
+  const [emotion, setEmotion] = useState("happy")
+  const [secondary, setSecondary] = useState("tired")
+  
+    useEffect(() => {
+      console.log(`It's ${emotion} around here! `)
+    },[] )
+  
+    useEffect(() => {
+      console.log(`It's ${secondary} around here! `)
+    },[] )
+
+  return(
+    <>
+    <h1>Current emotion is {emotion} and {secondary} </h1>
+    <button onClick={() => {
+      setEmotion("frustrated")
+    } } >Frustrated</button>
+
+    <button onClick={() => {
+      setEmotion("Enthusiastic")
+    } } >Enthusiastic</button>
+
+
+    <button onClick={() => {
+      setSecondary("frustrated")
+    } } >make Crabby</button>
+
+    <button onClick={() => {
+      setSecondary("Enthusiastic")
+    } } >make happy</button>
+
+
+    {/* {authorized? <SecretComponent />: <RegularComponent />} */}
+    </>
+  )
 }
 
 export default App;
